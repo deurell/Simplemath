45f0cbbbeec00d356f1a7ea807f20c34e6f1af27
Mikael Deurell <mikael@deurell.net>
2023-10-19 17:25:35 +0200

    support nil operands or result, added tests

--- Sources/Equation.swift
+++ Sources/Equation.swift
@@ -1,51 +1,160 @@
 enum Operation {
-    case addition, subtraction, multiplication, division
+  case addition, subtraction, multiplication, division
+}
+
+enum Operand {
+  case doubleValue(Double)
+  case rationalValue(Rational)
+}
+
+struct Rational {
+  var numerator: Int
+  var denominator: Int
+
+  init(_ numerator: Int, _ denominator: Int) {
+    assert(denominator != 0, "Denominator cannot be zero.")
+    self.numerator = numerator
+    self.denominator = denominator
+  }
+
+  func simplified() -> Rational {
+    let gcdValue = gcd(numerator, denominator)
+    return Rational(numerator / gcdValue, denominator / gcdValue)
+  }
+
+  private func gcd(_ a: Int, _ b: Int) -> Int {
+    return b == 0 ? a : gcd(b, a % b)
+  }
+
+  func add(_ other: Rational) -> Rational {
+    let newNumerator = self.numerator * other.denominator + other.numerator * self.denominator
+    let newDenominator = self.denominator * other.denominator
+    return Rational(newNumerator, newDenominator).simplified()
+  }
+
+  func subtract(_ other: Rational) -> Rational {
+    let newNumerator = self.numerator * other.denominator - other.numerator * self.denominator
+    let newDenominator = self.denominator * other.denominator
+    return Rational(newNumerator, newDenominator).simplified()
+  }
+
+  func multiply(_ other: Rational) -> Rational {
+    let newNumerator = self.numerator * other.numerator
+    let newDenominator = self.denominator * other.denominator
+    return Rational(newNumerator, newDenominator).simplified()
+  }
+
+  func divide(_ other: Rational) -> Rational {
+    assert(other.numerator != 0, "Cannot divide by zero.")
+    return self.multiply(Rational(other.denominator, other.numerator)).simplified()
+  }
 }
 
 struct Equation {
-    var operation: Operation
-    var operand1: Double?
-    var operand2: Double?
-    var result: Double?
-    
-    func solve() -> Double? {
-        switch operation {
-        case .addition:
-            if let op1 = operand1, let op2 = operand2 {
-                return op1 + op2
-            } else if let op1 = operand1, let res = result {
-                return res - op1
-            } else if let op2 = operand2, let res = result {
-                return res - op2
-            }
-            
-        case .subtraction:
-            if let op1 = operand1, let op2 = operand2 {
-                return op1 - op2
-            } else if let op1 = operand1, let res = result {
-                return op1 - res
-            } else if let op2 = operand2, let res = result {
-                return res + op2
-            }
-            
-        case .multiplication:
-            if let op1 = operand1, let op2 = operand2 {
-                return op1 * op2
-            } else if let op1 = operand1, let res = result, op1 != 0 {
-                return res / op1
-            } else if let op2 = operand2, let res = result, op2 != 0 {
-                return res / op2
-            }
-            
-        case .division:
-            if let op1 = operand1, let op2 = operand2, op2 != 0 {
-                return op1 / op2
-            } else if let op1 = operand1, let res = result, res != 0 {
-                return op1 / res
-            } else if let op2 = operand2, let res = result, op2 != 0 {
-                return res * op2
-            }
-        }
-        return nil
+  var operation: Operation
+  var operand1: Operand?
+  var operand2: Operand?
+  var result: Operand?
+
+  func solve() -> Operand? {
+    switch operation {
+    case .addition:
+      // Double Addition
+      if case .doubleValue(let op1)? = operand1,
+        case .doubleValue(let op2)? = operand2
+      {
+        return .doubleValue(op1 + op2)
+      }
+      // Rational Addition
+      else if case .rationalValue(let op1)? = operand1,
+        case .rationalValue(let op2)? = operand2
+      {
+        return .rationalValue(op1.add(op2))
+      }
+      // Addition with one operand missing (Double)
+      else if case .doubleValue(let op1)? = operand1,
+        case .doubleValue(let res)? = result
+      {
+        return .doubleValue(res - op1)
+      } else if case .doubleValue(let op2)? = operand2,
+        case .doubleValue(let res)? = result
+      {
+        return .doubleValue(res - op2)
+      }
+
+    case .subtraction:
+      // Double Subtraction
+      if case .doubleValue(let op1)? = operand1,
+        case .doubleValue(let op2)? = operand2
+      {
+        return .doubleValue(op1 - op2)
+      }
+      // Rational Subtraction
+      else if case .rationalValue(let op1)? = operand1,
+        case .rationalValue(let op2)? = operand2
+      {
+        return .rationalValue(op1.subtract(op2))
+      }
+      // Subtraction with one operand missing (Double)
+      else if case .doubleValue(let op1)? = operand1,
+        case .doubleValue(let res)? = result
+      {
+        return .doubleValue(op1 - res)
+      } else if case .doubleValue(let op2)? = operand2,
+        case .doubleValue(let res)? = result
+      {
+        return .doubleValue(res + op2)
+      }
+
+    case .multiplication:
+      // Double Multiplication
+      if case .doubleValue(let op1)? = operand1,
+        case .doubleValue(let op2)? = operand2
+      {
+        return .doubleValue(op1 * op2)
+      }
+      // Rational Multiplication
+      else if case .rationalValue(let op1)? = operand1,
+        case .rationalValue(let op2)? = operand2
+      {
+        return .rationalValue(op1.multiply(op2))
+      }
+      // Multiplication with one operand missing (Double)
+      else if case .doubleValue(let op1)? = operand1,
+        case .doubleValue(let res)? = result, op1 != 0
+      {
+        return .doubleValue(res / op1)
+      } else if case .doubleValue(let op2)? = operand2,
+        case .doubleValue(let res)? = result, op2 != 0
+      {
+        return .doubleValue(res / op2)
+      }
+
+    case .division:
+      // Double Division
+      if case .doubleValue(let op1)? = operand1,
+        case .doubleValue(let op2)? = operand2, op2 != 0
+      {
+        return .doubleValue(op1 / op2)
+      }
+      // Rational Division
+      else if case .rationalValue(let op1)? = operand1,
+        case .rationalValue(let op2)? = operand2
+      {
+        return .rationalValue(op1.divide(op2))
+      }
+      // Division with one operand missing (Double)
+      else if case .doubleValue(let op1)? = operand1,
+        case .doubleValue(let res)? = result, res != 0
+      {
+        return .doubleValue(op1 / res)
+      } else if case .doubleValue(let op2)? = operand2,
+        case .doubleValue(let res)? = result, op2 != 0
+      {
+        return .doubleValue(res * op2)
+      }
     }
+    return nil
+  }
+
 }
--- Tests/EquationTests.swift
+++ Tests/EquationTests.swift
@@ -1,25 +1,117 @@
 import XCTest
+
 @testable import equations
 
 class EquationTests: XCTestCase {
-    
-    func testAddition() {
-        let equation = Equation(operation: .addition, operand1: 5, operand2: 3, result: nil)
-        XCTAssertEqual(equation.solve(), 8)
+
+  func testAddition1() {
+    let equation = Equation(
+      operation: .addition,
+      operand1: .doubleValue(3.0),
+      operand2: .doubleValue(2.0),
+      result: nil)
+    if case .doubleValue(let resultValue)? = equation.solve() {
+      XCTAssertEqual(resultValue, 5.0)
+    } else {
+      XCTFail("Expected double result")
     }
-    
-    func testSubtraction() {
-        let equation = Equation(operation: .subtraction, operand1: 10, operand2: 4, result: nil)
-        XCTAssertEqual(equation.solve(), 6)
+  }
+
+  func testAddition2() {
+    let equation = Equation(
+      operation: .addition,
+      operand1: .rationalValue(Rational(5, 1)),
+      operand2: .rationalValue(Rational(3, 1)),
+      result: nil)
+    if case .rationalValue(let resultValue)? = equation.solve() {
+      XCTAssertEqual(resultValue.numerator, 8)
+      XCTAssertEqual(resultValue.denominator, 1)
+    } else {
+      XCTFail("Expected rational result")
     }
-    
-    func testMultiplication() {
-        let equation = Equation(operation: .multiplication, operand1: 6, operand2: 2, result: nil)
-        XCTAssertEqual(equation.solve(), 12)
+  }
+
+  func testAddition3() {
+    let equation = Equation(
+      operation: .addition,
+      operand1: .doubleValue(3.0),
+      operand2: nil,
+      result: .doubleValue(5.0))
+    if case .doubleValue(let resultValue)? = equation.solve() {
+      XCTAssertEqual(resultValue, 2.0)
+    } else {
+      XCTFail("Expected double result")
     }
-    
-    func testDivision() {
-        let equation = Equation(operation: .division, operand1: 8, operand2: 2, result: nil)
-        XCTAssertEqual(equation.solve(), 4)
+  }
+
+  func testSubtraction1() {
+    let equation = Equation(
+      operation: .subtraction,
+      operand1: .rationalValue(Rational(10, 1)),
+      operand2: .rationalValue(Rational(4, 1)),
+      result: nil)
+    if case .rationalValue(let resultValue)? = equation.solve() {
+      XCTAssertEqual(resultValue.numerator, 6)
+      XCTAssertEqual(resultValue.denominator, 1)
+    } else {
+      XCTFail("Expected rational result")
     }
+  }
+
+  func testMultiplication1() {
+    let equation = Equation(
+      operation: .multiplication,
+      operand1: .rationalValue(Rational(6, 2)),
+      operand2: .rationalValue(Rational(4, 2)),
+      result: nil)
+    if case .rationalValue(let resultValue)? = equation.solve() {
+      XCTAssertEqual(resultValue.numerator, 6)
+      XCTAssertEqual(resultValue.denominator, 1)
+    } else {
+      XCTFail("Expected rational result")
+    }
+  }
+
+  func testMultiplication2() {
+    let equation = Equation(
+      operation: .multiplication,
+      operand1: .rationalValue(Rational(2, 3)),
+      operand2: .rationalValue(Rational(4, 6)),
+      result: nil)
+    if case .rationalValue(let resultValue)? = equation.solve() {
+      XCTAssertEqual(resultValue.numerator, 4)
+      XCTAssertEqual(resultValue.denominator, 9)
+    } else {
+      XCTFail("Expected rational result")
+    }
+  }
+
+    func testMultiplication3() {
+    let equation = Equation(
+      operation: .multiplication,
+      operand1: .doubleValue(2.0),
+      operand2: nil,
+      result: .doubleValue(7.0))
+    if case .doubleValue(let resultValue)? = equation.solve() {
+      XCTAssertEqual(resultValue, 3.5)
+    } else {
+      XCTFail("Expected double result")
+    }
+  }
+
+  func testDivisionSimple() {
+    let equation = Equation(
+      operation: .division,
+      operand1: .rationalValue(Rational(6, 1)),  // which is 6
+      operand2: .rationalValue(Rational(2, 1)),  // which is 2
+      result: nil)
+    if case .rationalValue(let resultValue)? = equation.solve() {
+      print(resultValue.numerator, resultValue.denominator)  // should print 3 1
+      XCTAssertEqual(resultValue.numerator, 3)  // because 6 divided by 2 is 3
+      XCTAssertEqual(resultValue.denominator, 1)  // simplified denominator
+    } else {
+      XCTFail("Expected rational result")
+    }
+  }
+
 }
