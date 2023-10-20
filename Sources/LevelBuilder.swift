dd815f5ffd7ccae1ff476262008675353bb1438a
Mikael Deurell <mikael@deurell.net>
2023-10-20 14:40:03 +0200

    formatting

--- Sources/LevelBuilder.swift
+++ Sources/LevelBuilder.swift
@@ -1,68 +1,68 @@
 import Foundation
 
 struct Choice {
-    let text: String
-    let image: String
+  let text: String
+  let image: String
 }
 
 struct Question {
-    let text: String
-    let image: String
-    var equation: Equation?
-    let choices: [Choice]
-
-    init(text: String, image: String, equation: Equation? = nil, choices: [Choice]) {
-        self.text = text
-        self.image = image
-        self.equation = equation
-        self.choices = choices
-    }
-
-    func withEquation(_ equation: Equation) -> Question {
-        var updatedQuestion = self
-        updatedQuestion.equation = equation
-        return updatedQuestion
-    }
+  let text: String
+  let image: String
+  var equation: Equation?
+  let choices: [Choice]
+  
+  init(text: String, image: String, equation: Equation? = nil, choices: [Choice]) {
+    self.text = text
+    self.image = image
+    self.equation = equation
+    self.choices = choices
+  }
+  
+  func withEquation(_ equation: Equation) -> Question {
+    var updatedQuestion = self
+    updatedQuestion.equation = equation
+    return updatedQuestion
+  }
 }
 
 @resultBuilder
 struct QuestionBuilder {
-    static func buildBlock(_ components: Question...) -> [Question] {
-        return components
-    }
+  static func buildBlock(_ components: Question...) -> [Question] {
+    return components
+  }
 }
 
 func MathQuestion(
-    _ text: String, image: String,
-    @ChoiceBuilder choices: () -> [Choice]
+  _ text: String, image: String,
+  @ChoiceBuilder choices: () -> [Choice]
 ) -> Question {
-    return Question(text: text, image: image, choices: choices())
+  return Question(text: text, image: image, choices: choices())
 }
 
 @resultBuilder
 struct ChoiceBuilder {
-    static func buildBlock(_ components: Choice...) -> [Choice] {
-        return components
-    }
+  static func buildBlock(_ components: Choice...) -> [Choice] {
+    return components
+  }
 }
 
 func MathChoice(_ text: String, image: String) -> Choice {
-    return Choice(text: text, image: image)
+  return Choice(text: text, image: image)
 }
 
 @resultBuilder
 struct LevelBuilder {
-    static func buildBlock(_ components: Question...) -> [Question] {
-        return components
-    }
+  static func buildBlock(_ components: Question...) -> [Question] {
+    return components
+  }
 }
 
 func Level(@LevelBuilder builder: () -> [Question]) -> [Question] {
-    return builder()
+  return builder()
 }
 
 func MathEquation(
-    _ operation: Operation, operand1: Operand?, operand2: Operand?, result: Operand?
+  _ operation: Operation, operand1: Operand?, operand2: Operand?, result: Operand?
 ) -> Equation {
-    return Equation(operation: operation, operand1: operand1, operand2: operand2, result: result)
+  return Equation(operation: operation, operand1: operand1, operand2: operand2, result: result)
 }
